1184986834bc0addc022a8a06f5603a6a362bd05
Matheus <matheusmgomes09@gmail.com>
2023-01-17 12:36:20 -0300

    design system project initialized

---
+++ src/components/AbButton/StyledButton.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background: #eb9b00;
+  padding: 1rem 2rem;
+  border: 2px solid #eb9b00;
+  color: #ffffff;
+  font-size: 1.25rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #b87900;
+    border: 2px solid #b87900;
+  }
+`;
+
+export default StyledButton;
---
+++ src/components/AbButton/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import StyledButton from './StyledButton';
+
+export function AbButton() {
+  return <StyledButton>Clique aqui</StyledButton>;
+}
