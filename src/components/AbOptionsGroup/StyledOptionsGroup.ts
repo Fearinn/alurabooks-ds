0ff8bc2dd755bbba5feafee3e58f747d753b642d
Vivih Brizolla <matheusmgomes09@gmail.com>
2023-01-20 17:00:09 -0300

    [feat] AbOptionsGroup component added

---
+++ src/components/AbOptionsGroup/StyledOptionsGroup.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import { IOption } from '../../interfaces/Option';
+
+export const StyledOptionsList = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  font-family: 'Poppins', sans-serif;
+  gap: 1.25rem;
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export const StyledOption = styled.div<IOption>`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: ${(props: IOption) => props.selected ? "linear-gradient(97.54deg, #002F52 35.49%, #326589 165.37%)" : "#FFFFFF"};
+    border: 1px solid;
+    border-color: ${(props: IOption) => props.selected ? "linear-gradient(97.54deg, #002F52 35.49%, #326589 165.37%)" : "#eb9b00"};
+    border-radius: 8px;
+    cursor: pointer;
+    gap: 0.5rem;
+    width: 194px;
+    height: 88px;
+    padding: 0.5rem 0;
+    font-size: 0.75rem;
+    color: ${(props: IOption) => props.selected ? "#FFFFFF" : "#eb9b00"};
+
+    h3 {
+      text-transform: capitalize;
+    }
+    strong {
+      font-size: 1rem;
+      font-weight: 700;
+    }
+
+    span:not(:has(strong)) {
+      color: rgba(0, 0, 0, 0.54);
+    }
+`;
