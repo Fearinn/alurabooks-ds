afe75029f93b7f32c0b8e2414dd1e9bf01a81684
Fearinn <matheusmgomes09@gmail.com>
2023-01-24 11:05:24 -0300

    AbTag text prop replaced by required children

--- src/components/AbTag/index.tsx
+++ src/components/AbTag/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import IAbTag from '../../interfaces/Tag';
 import StyledTag from './StyledTag';
 
-export function AbTag({ text = "I'm a tag" }: IAbTag) {
-  return <StyledTag>{text}</StyledTag>;
+export function AbTag({ children }: IAbTag) {
+  return <StyledTag>{children}</StyledTag>;
 }
