bc40d10f9390219685c6fd651999cfa1a324ef73
Fearinn <matheusmgomes09@gmail.com>
2023-02-06 16:57:47 -0300

    css: secondary AbButton background-color changed to transparent

--- src/components/AbButton/StyledButton.ts
+++ src/components/AbButton/StyledButton.ts
@@ -4,7 +4,7 @@ import IAbButton from '../../interfaces/Button';
 const StyledButton = styled.button<IAbButton>`
   box-sizing: border-box;
   background: ${(props: IAbButton) =>
-    props.customType === 'primary' ? '#eb9b00' : '#FFFFFF'};
+    props.customType === 'primary' ? '#eb9b00' : 'transparent'};
   padding: 1rem 2rem;
   border: 2px solid #eb9b00;
   color: ${(props: IAbButton) =>
@@ -22,7 +22,6 @@ const StyledButton = styled.button<IAbButton>`
         `
       : css`
           &:hover {
-            background-color: #FFFFFF;
             border: 2px solid #b87900;
             color: #b87900;
           }
