f73a2c01eeb2fc73cbaea95c6a2c5ec2afa3cdb6
Fearinn <matheusmgomes09@gmail.com>
2023-02-07 19:54:07 -0300

    refactor: click event listener to close modal moved from document to modalBox ref

--- src/components/AbModal/index.tsx
+++ src/components/AbModal/index.tsx
@@ -1,43 +1,56 @@
-import React, { useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 import IAbModal from '../../interfaces/Modal';
 import StyledModal from './StyledModal';
 
-export function AbModal({ title, children, htmlId, whenClosed, open}: IAbModal) {
-  const modalRef = useRef<HTMLDivElement>(null);
+export function AbModal({
+  title,
+  children,
+  htmlId,
+  whenClosed,
+  open,
+}: IAbModal) {
+  const modalBoxRef = useRef<HTMLDivElement>(null);
+  const modalBgRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = (event: Event) => {
+    if (
+      event.target instanceof Node &&
+      !modalBoxRef.current?.contains(event.target)
+    ) {
+      whenClosed(false);
+    }
+  };
 
   useEffect(() => {
-    if (modalRef.current) {
-      modalRef.current.setAttribute('tabindex', '0');
-      modalRef.current.addEventListener('focusout', (event) => {
+    if (modalBoxRef.current) {
+      modalBoxRef.current.setAttribute('tabindex', '0');
+      modalBoxRef.current.addEventListener('focusout', (event) => {
         if (
           event.relatedTarget instanceof Node &&
-          !modalRef.current?.contains(event.relatedTarget)
+          !modalBoxRef.current?.contains(event.relatedTarget)
         )
-          modalRef.current?.focus();
+          modalBoxRef.current?.focus();
       });
     }
 
-    document.addEventListener('click', (event) => {
-      if (
-        event.target instanceof Node &&
-        !modalRef.current?.contains(event.target)
-      ) {
-        whenClosed(false)
-      }
-    });
+    modalBgRef.current?.addEventListener('click', handleClick);
+
+    return () => {
+      modalBgRef.current?.removeEventListener('click', handleClick);
+    };
   }, []);
 
   return (
     <>
       {open && (
-        <StyledModal role="dialog" aria-labelledby={htmlId}>
-          <div className="abmodal-box" ref={modalRef}>
+        <StyledModal role="dialog" aria-labelledby={htmlId} ref={modalBgRef}>
+          <div className="abmodal-box" ref={modalBoxRef}>
             <header>
               <h2 id={htmlId}>{title}</h2>
               <button
                 aria-label="fechar modal"
                 type="button"
-                onClick={() => whenClosed(false)}
+                // onClick={() => whenClosed(false)}
               >
                 X
               </button>
