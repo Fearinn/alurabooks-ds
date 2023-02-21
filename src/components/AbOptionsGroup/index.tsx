2848826810ee49cf7bed1e0df8480613ca4fb890
Fearinn <matheusmgomes09@gmail.com>
2023-02-21 13:31:11 -0300

    refactor: book price formated with Intl

--- src/components/AbOptionsGroup/index.tsx
+++ src/components/AbOptionsGroup/index.tsx
@@ -9,24 +9,30 @@ export function AbOptionsGroup({
   const [selected, setSelected] = useState<IAbOption | null>(defaultOption);
   return (
     <StyledOptionsList role="listbox">
-      {options.map((option) => (
-        <StyledOption
-          {...option}
-          key={option.identificator}
-          role="option"
-          aria-selected={selected?.identificator === option.identificator}
-          selected={selected?.identificator === option.identificator}
-          onClick={() => {
-            setSelected(option);
-          }}
-        >
-          <h3>{option.title}</h3>
-          <span>
-            <strong>R${option.price.toFixed(2)}</strong>
-          </span>
-          <span>{option.formats}</span>
-        </StyledOption>
-      ))}
+      {options.map((option) => {
+        const price = Intl.NumberFormat('pt-br', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(option.price);
+        return (
+          <StyledOption
+            {...option}
+            key={option.identificator}
+            role="option"
+            aria-selected={selected?.identificator === option.identificator}
+            selected={selected?.identificator === option.identificator}
+            onClick={() => {
+              setSelected(option);
+            }}
+          >
+            <h3>{option.title}</h3>
+            <span>
+              <strong>{price}</strong>
+            </span>
+            <span>{option.formats}</span>
+          </StyledOption>
+        );
+      })}
     </StyledOptionsList>
   );
 }
